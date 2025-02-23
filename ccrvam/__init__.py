cf23f13e7e551ec5de5f799cccd25e7415241f80
Dhyey Mavani <82772894+DhyeyMavani2003@users.noreply.github.com>
2025-02-23 16:11:31 -0500

    Add example documentation, update requirements, and implement utility functions

--- ccrvam/__init__.py
+++ ccrvam/__init__.py
@@ -1,4 +1,20 @@
 """
-  A very stupid example module, mainly for showing all capabilities of PyPiTemplate.
+  Python implementation of Checkerboard Copula Regression-based Visualization and Association Measure
 """
-__version__ = "0.0.1"
+from ccrvam.checkerboard.gencopula import GenericCheckerboardCopula
+from ccrvam.checkerboard.utils import gen_contingency_to_case_form, gen_case_form_to_contingency
+from ccrvam.checkerboard.genstatsim import (
+        bootstrap_ccram,
+        bootstrap_predict_category_summary,
+        permutation_test_ccram,
+    )
+
+__version__ = "0.1.0"
+__all__ = [
+  "GenericCheckerboardCopula",
+  "gen_contingency_to_case_form",
+  "gen_case_form_to_contingency",
+  "bootstrap_ccram",
+  "bootstrap_predict_category_summary",
+  "permutation_test_ccram",
+]
