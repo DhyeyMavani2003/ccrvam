5723d6bc13efe023483f57c71b882cb01b431566
Dhyey Mavani <82772894+DhyeyMavani2003@users.noreply.github.com>
2025-03-09 22:30:27 -0400

    update pkg version to 0.7.2

--- ccrvam/__init__.py
+++ ccrvam/__init__.py
@@ -9,7 +9,7 @@ from ccrvam.checkerboard.genstatsim import (
         permutation_test_ccram,
     )
 
-__version__ = "0.7.1"
+__version__ = "0.7.2"
 __all__ = [
   "GenericCCRVAM",
   "DataProcessor",
